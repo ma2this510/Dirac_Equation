ee6bbd87d3ac50b8ef365a0b5f0b0c066e8c032e
Mathis PANET <mathispanet@gmail.com>
2025-03-10 10:57:41 +0100

    Total MP working version (but missing the first linked state)

--- main.f90
+++ main.f90
@@ -3,19 +3,19 @@ program main
    use bspline_mod
    implicit none
 
-   integer :: d, n, iter, nprime, maxit, rn, i_tmp, j_tmp
-   type(mp_real) :: amin, amax, Z, C, kappa, eps, w, ga, ext, kappa_abs, zqvdn
-   type(mp_real), dimension(:, :), allocatable :: A, B
-   type(mp_real), dimension(:), allocatable :: work, wa, avec, bvec
+   integer :: d, n, nprime, i_tmp, ierr
+   type(mp_real) :: amin, amax, Z, C, kappa
+   type(mp_real), dimension(:, :), allocatable :: A, B, vect
+   type(mp_real), dimension(:), allocatable :: w, fv1, fv2
 
    !----------------------------------------------------------------
    ! Define Important Variables
-   d = 7 ! Order of Mathemathica + 1
-   n = 40 ! Number of B-Splines
-   Z = mpreal(1.d0)
+   d = 9 ! Order of Mathemathica + 1
+   n = 80 ! Number of B-Splines
+   Z = mpreal(2.d0)
    C = mpreal(137.036d0) ! check CODATA 1986
    kappa = mpreal(-1.d0)
-   amin = mpreal(0.1d0)
+   amin = mpreal(0.002d0)
    amax = mpreal(10.d0)
 
    nprime = n - 2
@@ -27,54 +27,26 @@ program main
    call matrixAB(d, n, Z, kappa, C, amin, amax, A, B, .true.)
 
    !----------------------------------------------------------------
-   ! Generate Exact Solution
-
-   print *, 'Calculating Exact Solution'
-
-   rn = mpreal(1.d0)
-   kappa_abs = abs(kappa)
-   ga = (kappa_abs**mpreal(2.d0) - (Z/C)**mpreal(2.d0))**mpreal(0.5d0)
-   ext = (c**mpreal(2.d0))*(1 + (Z/(C*(rn - kappa_abs + ga)))**mpreal(2.d0))**mpreal(-0.5d0) - (c**mpreal(2.d0))
-
-   zqvdn = mpreal(0.99d0)
-   w = ext*zqvdn
-
-   !----------------------------------------------------------------
    ! Get Eigenvalues
 
    print *, 'Calculating Eigenvalues'
 
+   allocate (w(2*nprime))
+   allocate (fv1(2*nprime))
+   allocate (fv2(2*nprime))
+   allocate (vect(2*nprime, 2*nprime))
+
+   call rsg(2*nprime, 2*nprime, A, B, w, 0, vect, fv1, fv2, ierr)
+
    open (2, file='eigenvalues.dat', status='replace')
 
-   allocate (avec(2*nprime*(2*nprime + 1)/2))
-   allocate (bvec(2*nprime*(2*nprime + 1)/2))
-
-   avec = mpreal(0.d0)
-   bvec = mpreal(0.d0)
-
    do i_tmp = 1, 2*nprime
-      do j_tmp = 1, i_tmp
-         avec((i_tmp*(i_tmp - 1))/2 + j_tmp) = A(i_tmp, j_tmp)
-         bvec((i_tmp*(i_tmp - 1))/2 + j_tmp) = B(i_tmp, j_tmp)
-      end do
+      call mpwrite(2, 30, 10, w(i_tmp))
    end do
+   
+   close (2)
 
-   allocate (wa(3*2*nprime + 1))
-   allocate (work(2*nprime))
-   work = mpreal(0.d0)
-   work(1) = mpreal(1.d0)
-   eps = mpreal(0.d-28)
-   maxit = 10
-
-   print *, 'Working'
-   do iter = 1, 2*nprime
-      call invsg(avec, bvec, 2*nprime, w, work, eps, 0, maxit, wa)
-      print *, "Eigenvalue", iter
-      call mpwrite(2, 30, 10, w)
-   end do
-   close (1)
-
-   deallocate (A, B, work, wa)
+   deallocate (A, B, fv1, fv2, w)
 
 end program main
 
