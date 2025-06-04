9baf4f3405ef7eddc3fdf986d9be4830af0bf631
Mathis PANET <mathis.panet@gmail.com>
2025-06-04 14:39:08 +0200

    add new result of order 5

--- coefv2.f90
+++ coefv2.f90
@@ -782,7 +782,7 @@ contains
 
       print *, "Error code: ", ierr
 
-      write (log_file, '(a,I4,a,I2,a)') "./result_spurious/error_", n, "_", d, "_5.txt"
+      write (log_file, '(a,I4,a,I2,a)') "./result_DKB/error_", n, "_", d, ".txt"
 
       open (2, file=log_file)
 
@@ -811,7 +811,7 @@ contains
 
       print *, "Errors written to ", log_file
 
-      write (log_file, '(a,I4,a,I2,a)') "./result_spurious/eigenvalues_", n, "_", d, "_5.txt"
+      write (log_file, '(a,I4,a,I2,a)') "./result_DKB/eigenvalues_", n, "_", d, ".txt"
 
       open (2, file=log_file)
 
--- main.f90
+++ main.f90
@@ -16,11 +16,11 @@ program main
    ! nmin = 20 ! Number minimal of B-Spline
    ! nmax = 90 ! Number minimal of B-Spline
    ! step = 5 ! Number of Intermedier n
-   n = 60 ! Number of B-Spline
+   n = 900 ! Number of B-Spline
    n_remove = 1 ! Number of Bspline to remove at the start and the end
    Z = '2.d0'
    C = '137.0359895d0' ! check CODATA 1986
-   kappa = '5.d0'
+   kappa = '-1.d0'
    amin = '1.d-6'
    amax = '2.d2'
    !-----------------------------------------------------------------!
